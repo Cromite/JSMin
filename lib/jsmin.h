5ccb0996af03eb0043117b19fcfe28afaf39b487
Cromite <120804897+Cromite@users.noreply.github.com>
2023-01-07 04:47:11 +0900

    Add files via upload

---
+++ lib/jsmin.h
@@ -0,0 +1,22 @@
+/**
+ * lib-jsmin v1.4.19
+ *
+ * Copyright (C) 2022-2023 Cromite.
+ * Released under the MIT license.
+ * see https://opensource.org/licenses/MIT
+ * 
+ * Release-Date: 2023-1-6
+ * Bug reports to <admin@cromite.net>
+ */
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+/**
+ * Minify javascript code
+ * @param code Original javascript code
+ * @return Minified javascript code
+ */
+char *jsmin(char *code);
+
+#endif
